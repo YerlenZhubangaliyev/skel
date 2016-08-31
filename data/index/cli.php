e397ccc5c6354afdfb0f5c79cba9ec9d9235c55c
Yerlen Zhubangaliyev <yerlen@yerlen.com>
2016-08-31 17:47:47 +0600

    default env

--- data/index/cli.php
+++ data/index/cli.php
@@ -1,9 +1,10 @@
 #!/usr/bin/env php
 <?php
-defined('APPLICATION') or define('APPLICATION', 'Cli');
-defined('ENVIRONMENT') or define('ENVIRONMENT', 'Development');
 defined('ROOT_DIR') or define('ROOT_DIR', __DIR__ . '/..');
 
 $loader = require_once(ROOT_DIR . '/vendor/autoload.php');
+
+defined('APPLICATION') or define('APPLICATION', 'Cli');
+defined('ENVIRONMENT') or define('ENVIRONMENT', (getenv('ENVIRONMENT')) ? getenv('ENVIRONMENT') : App::ENV_LOCAL);
 
 (new App($loader));
--- data/index/public.php
+++ data/index/public.php
@@ -1,9 +1,11 @@
 <?php
-defined('APPLICATION') or define('APPLICATION', getenv('APPLICATION'));
-defined('ENVIRONMENT') or define('ENVIRONMENT', getenv('ENVIRONMENT'));
 defined('ROOT_DIR') or define('ROOT_DIR', __DIR__ . '/..');
 
 $loader = require_once(ROOT_DIR . '/vendor/autoload.php');
+
+defined('APPLICATION') or define('APPLICATION', getenv('APPLICATION'));
+defined('ENVIRONMENT') or define('ENVIRONMENT', (getenv('ENVIRONMENT')) ? getenv('ENVIRONMENT') : App::ENV_LOCAL);
+
 
 try {
     (new App($loader));
