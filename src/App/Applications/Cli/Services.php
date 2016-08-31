4f75f613a8fb230a3df82ad34efe3e8981eda538
Yerlen Zhubangaliyev <yerlen@yerlen.com>
2016-08-31 11:05:15 +0600

    elasticsearch service

--- src/App/Applications/Cli/Services.php
+++ src/App/Applications/Cli/Services.php
@@ -2,6 +2,7 @@
 namespace App\Applications\Cli;
 
 use App\Abstractions\Services as BaseServices;
+use Elasticsearch;
 use Phalcon\Cli\Router as PhalconRouter;
 use Phalcon\Cli\Dispatcher as PhalconDispatcher;
 
@@ -21,6 +22,7 @@ final class Services extends BaseServices
         'db',
         'logger',
         'template',
+        'elastic',
     ];
 
     /**
@@ -39,5 +41,28 @@ final class Services extends BaseServices
         $this->di->set('router', function () {
             return new PhalconRouter();
         }, true);
+    }
+
+    /**
+     * ElasticSearch
+     */
+    protected function setElastic()
+    {
+        $this->di->set(
+            'elastic',
+            function () {
+                if (isset($this->di->getConfig()->elastic->hosts)) {
+                    $elastic = Elasticsearch\ClientBuilder
+                        ::create()
+                        ->setHosts($this->di->getConfig()->elastic->hosts)
+                        ->setLogger($this->di->getLogger())
+                        ->build()
+                    ;
+
+                    return $elastic;
+                }
+            },
+            true
+        );
     }
 }
