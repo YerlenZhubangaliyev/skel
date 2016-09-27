60e30e89a311949ec4cb87aabd39588dd12ae758
Yerlen Zhubangaliyev <yerlen@yerlen.com>
2016-09-27 17:23:13 +0600

    osx dev env init

---
+++ bin/dev/osx/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Create local server
+# OSX, brew
+
+source "$(dirname ${BASH_SOURCE[0]})/../../variables.sh"
+
+NGINX_CONF_DIR=/usr/local/etc/nginx/servers/
+PHP_VER=7.0
+PHP_CONF_DIR=/usr/local/etc/php/7.0/php-fpm.d/
+WEB_ROOT=/
